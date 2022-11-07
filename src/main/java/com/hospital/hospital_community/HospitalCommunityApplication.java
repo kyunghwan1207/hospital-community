fc3fd28a0f4414aac5211318d688330297e4c7d6
kyunghwan1207 <kyunghwanko1207@gmail.com>
2022-11-07 11:20:53 +0900

    hospital-community 빌드

---
+++ src/main/java/com/hospital/hospital_community/HospitalCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/hospital_community/HospitalCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
